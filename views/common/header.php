638574ddc2377c8de6fdba84cc7081278940a761
madmoronse <smolyaninov.se@yandex.ru>
2021-03-03 12:37:17 +0700

    create header

--- views/common/header.php
+++ views/common/header.php
@@ -10,15 +10,25 @@
 <body>
 <div id="header">
     <div class="top">
-        <div class="social"></div>
-        <div class="logo"></div>
-        <div class="callback"></div>
+        <div class="social">
+            <a href="#"><img src="image/icons/instagram.png" alt="инстаграм"></a>
+            <a href="#"><img src="image/icons/whatsapp.png" alt="whatsapp"></a>
+            <a href="#"><img src="image/icons/telegram.png" alt="telegram"></a>
+        </div>
+        <div class="logo">
+            <img src="image/logo.png" alt="логотип">
+        </div>
+        <div class="callback">
+            <a href="#"><button class="button">Заказать звонок</button></a>
+        </div>
     </div>
     <div class="menu">
         <ul>
             <?php foreach ($category as $item) {?>
                 <a href="#"> <li> <?php echo $item['title']; ?> </li></a>
             <?php }?>
+            <li>Отзывы</li>
+            <li>Контакты</li>
         </ul>
     </div>
 </div>
