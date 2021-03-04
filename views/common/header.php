0f6eca6d549e99f9e8b07e58173056333af23d45
madmoronse <smoljaninov.se@yandex.ru>
2021-03-04 23:09:30 +0700

    add boostrap

--- views/common/header.php
+++ views/common/header.php
@@ -2,14 +2,31 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="../css/style.css">
     <title> <?php echo $title; ?> </title>
 </head>
 <body>
-    <h1>Шапка!</h1>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    	<div class="container">
+    		<a href="#" class="navbar-brand">Пекарня</a>
+    		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false">
+    			<span class="navbar-toggler-icon"></span>
+    		</button>
+    		<div class="collapse navbar-collapse" id="navbarContent">
+    			<ul class="navbar-nav ml-auto">
+    				<?php foreach($category as $item) { ?>
+	    				<li class="nav-item">
+	    					<a href="" class="nav-link"><?php echo $item['title']; ?></a>
+	    				</li>
+    				<?php } ?>
+    			</ul>
+    		</div>
+    	</div>
+    </nav>
 
-    
 
-    
+
+
--- views/common/home.php
+++ views/common/home.php
@@ -1,5 +1,45 @@
 <?php echo $header; ?>
 
-<h2>Главная страница</h2>
+<main>
+	<div class="carousel slide" id="carouselControls" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="../image/intro/slide-1.jpg">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="../image/intro/slide-2.jpg">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="../image/intro/slide-3.jpg">
+			</div>
+		</div>
+		<a class="carousel-control-prev" role="button" href="#carouselControls" data-slide="prev">
+			<span class="carousel-control-prev-icon"></span>
+		</a>
+		<a class="carousel-control-next" role="button" href="#carouselControls" data-slide="next">
+			<span class="carousel-control-next-icon"></span>
+		</a>
+	</div>
+
+	<div class="container my-5">
+		<div class="row">
+			<div class="col-md-7 col-sm-12">
+				<img class="w-100" src="../image/intro/slide-2.jpg">
+			</div>
+
+			<div class="col-md-5 col-sm-12">
+				<h2>Торты</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+
+		</div>
+	</div>
+
+</main>
 
 <?php echo $footer; ?>
