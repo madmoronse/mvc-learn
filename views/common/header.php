2239fa06c58240cfb6f0a338a64644fe450bf04f
madmoronse <smolyaninov.se@yandex.ru>
2021-03-09 18:00:34 +0700

    change intro

--- views/common/header.php
+++ views/common/header.php
@@ -2,9 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script type="text/javascript" src="https://yastatic.net/jquery/2.1.3/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="../css/style.css">
     <title> <?php echo $title; ?> </title>
@@ -20,7 +18,6 @@
 		</a>
 	</nav>
 <div id="head">
-	<div class="nav">
 	<div class="header">
 		<div class="logo">
 			<img src="../../image/logo.png">
@@ -45,7 +42,6 @@
 			<span>0</span>
 		</div>
 	</div>
-</div>
 </div>
 <div class="social" style="display: none;">
 	<img src="../../image/icons/telegram.png">
