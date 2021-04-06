0241a07e6dcfcc5f83c18d84900361472b4a9080
madmoronse <smolyaninov.se@yandex.ru>
2021-04-06 14:32:21 +0700

    delete pdo

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 ini_set('display_error', 1);
 require_once('system/startup.php');
 
-$db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-$db->exec('SET CHARACTER SET utf8');
-
-
 $router = new Router($registry);
 $registry->set('router', $router);
 
